f4180f9d2915e9aad4ee42c08bd5336d5d67a5ac
Zigrik <lm19-kid@mail.ru>
2023-02-14 15:15:19 +0600

    max

---
+++ max min/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите три числа");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+if(a>b)
+{
+    if(a>c)
+    {
+        Console.WriteLine("max "+a);
+    }
+    else
+    {
+        Console.WriteLine("max "+c);
+    }
+}
+else if(b>c)
+{
+    Console.WriteLine("max "+b);
+}
+else
+{
+    Console.WriteLine("max "+c);
+}
