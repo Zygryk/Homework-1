976d3688391a91dd1dd16582bc4e129c633b4854
Zigrik <lm19-kid@mail.ru>
2023-02-14 14:10:51 +0600

    четное/нечетное число

---
+++ четное/нечетное число/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int value = int.Parse(Console.ReadLine());
+if((value%2)==0)
+{
+    Console.WriteLine("Четное число");
+}
+else
+{
+    Console.WriteLine("Нечетное число");
+}
